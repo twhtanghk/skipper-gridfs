7535c0effb04f06efbae9ba9ca1d66723396bc65
Tommy Tang <twhtanghk@gmail.com>
2016-06-13 17:32:57 +0800

    update adapter method to retrun promise instead of using node callback

--- index.coffee
+++ index.coffee
@@ -36,103 +36,76 @@ module.exports = (opts) ->
 		scheme:	'mongodb'
 		bucket:	GridStore.DEFAULT_ROOT_COLLECTION
 		port:	27017
-	self.conn = new Promise (fulfill, reject) ->
-		client.connect self.opts.uri || mongodbUri.format(self.opts)
-			.then (db) ->
-				self.db = db
-				self.gfs = Grid self.db, server
-				fulfill()
-			.catch reject
+	self.conn = client.connect self.opts.uri || mongodbUri.format(self.opts)
+		.then (db) ->
+			self.db = db
+			self.gfs = Grid self.db, server
+			Promise.resolve()
 				
-	ls: (dirname, cb) ->
-		ret = new Promise (fulfill, reject) ->
-			self.conn
-				.then ->
-					index =	
-						filename: 	1
-						uploadDate: -1
-					self.gfs.collection(self.opts.bucket).ensureIndex index, (err, indexName) ->
-						if err
-							return reject err
-						self.gfs.collection	self.opts.bucket
-							.distinct 'filename', 'metadata.dirname': dirname, (err, files) ->
-	                        	if err 
-	                        		return reject err
-	                        	fulfill files
-				.catch reject
-				
-		if cb
-			ret.nodeify(cb)
-			return @
-		
-		return ret
-                
-	read: (fd, cb) ->
+	ls: (dirname) ->
+		self.conn
+			.then ->
+				index =	
+					filename: 	1
+					uploadDate: -1
+				self.gfs.collection(self.opts.bucket).ensureIndex index, (err, indexName) ->
+					if err
+						return Promise.reject err
+					self.gfs.collection	self.opts.bucket
+						.distinct 'filename', 'metadata.dirname': dirname, (err, files) ->
+                        	if err 
+                        		return Promise.reject err
+                        	Promise.resolve files
+	        
+	read: (fd) ->
 		@readLastVersion fd, cb
 						
-	readLastVersion: (fd, cb) ->
+	readLastVersion: (fd) ->
 		@readVersion fd, -1, cb
 		
-	readVersion: (fd, version, cb) ->
-		ret = new Promise (fulfill, reject) ->
-			self.conn
-				.then ->
-					self.gfs
-						.collection self.opts.bucket
-						.find filename: fd
-						.limit -1
-						.skip if version < 0 then Math.abs(version) - 1 else version
-						.sort { uploadDate: if version < 0 then -1 else 1 } 
-						.next (err, file) ->
-							if err
-								console.log err
-								return reject err
-
-							if !file
-								err = new Error('ENOENT')
-								_.extend err,
-									name:		'Error (ENOENT)'
-									code:		'ENOENT'
-									status:		404
-									message:	"No file exists in this mongo gridfs bucket with that file descriptor (#{fd})"
-								return reject err
-	                    
-							gridStore = new GridStore self.db, file._id, 'r', root: self.opts.bucket
-							gridStore.open (err, gridStore) ->
-								if err
-									return reject err
-                            
-								stream = gridStore.stream()
-								stream.pipe concat fulfill
-								stream.on 'error', reject
-				.catch reject
-				
-		if cb
-			ret.nodeify(cb)
-			return @
-			
-		return ret
-		
-	rm: (fd, cb) ->
-		ret = new Promise (fulfill, reject) ->
-			self.conn
-				.then ->
-					self.gfs.exist {filename: fd, root: self.opts.bucket}, (err, found) ->
+	readVersion: (fd, version) ->
+		self.conn
+			.then ->
+				self.gfs
+					.collection self.opts.bucket
+					.find filename: fd
+					.limit -1
+					.skip if version < 0 then Math.abs(version) - 1 else version
+					.sort { uploadDate: if version < 0 then -1 else 1 } 
+					.next (err, file) ->
 						if err
-							return reject err
-						if not found
-							return reject "#{fd} not found"
-						self.gfs.remove {filename: fd, root: self.opts.bucket}, (err) ->
+							return Promise.reject err
+
+						if !file
+							err = new Error('ENOENT')
+							_.extend err,
+								name:		'Error (ENOENT)'
+								code:		'ENOENT'
+								status:		404
+								message:	"No file exists in this mongo gridfs bucket with that file descriptor (#{fd})"
+							return Promise.reject err
+                    
+						gridStore = new GridStore self.db, file._id, 'r', root: self.opts.bucket
+						gridStore.open (err, gridStore) ->
 							if err
 								return reject err
-							fulfill()
-				.catch reject
-	
-		if cb
-			ret.nodeify(cb)
-			return @
-		
-		return ret
+                        
+							stream = gridStore.stream()
+							stream.pipe concat Promise.resolve
+							stream.on 'error', Promise.reject
+				
+	rm: (fd) ->
+		self.conn
+			.then ->
+				self.gfs.exist {filename: fd, root: self.opts.bucket}, (err, found) ->
+					if err
+						return Promise.reject err
+					if not found
+						return Promise.reject "#{fd} not found"
+					self.gfs.remove {filename: fd, root: self.opts.bucket}, (err) ->
+						if err
+							return Promise.reject err
+						Promise.resolve()
 			
 	receive: (opts) ->
 		
--- index.js
+++ index.js
@@ -54,125 +54,98 @@
       bucket: GridStore.DEFAULT_ROOT_COLLECTION,
       port: 27017
     });
-    self.conn = new Promise(function(fulfill, reject) {
-      return client.connect(self.opts.uri || mongodbUri.format(self.opts)).then(function(db) {
-        self.db = db;
-        self.gfs = Grid(self.db, server);
-        return fulfill();
-      })["catch"](reject);
+    self.conn = client.connect(self.opts.uri || mongodbUri.format(self.opts)).then(function(db) {
+      self.db = db;
+      self.gfs = Grid(self.db, server);
+      return Promise.resolve();
     });
     return {
-      ls: function(dirname, cb) {
-        var ret;
-        ret = new Promise(function(fulfill, reject) {
-          return self.conn.then(function() {
-            var index;
-            index = {
-              filename: 1,
-              uploadDate: -1
-            };
-            return self.gfs.collection(self.opts.bucket).ensureIndex(index, function(err, indexName) {
+      ls: function(dirname) {
+        return self.conn.then(function() {
+          var index;
+          index = {
+            filename: 1,
+            uploadDate: -1
+          };
+          return self.gfs.collection(self.opts.bucket).ensureIndex(index, function(err, indexName) {
+            if (err) {
+              return Promise.reject(err);
+            }
+            return self.gfs.collection(self.opts.bucket).distinct('filename', {
+              'metadata.dirname': dirname
+            }, function(err, files) {
               if (err) {
-                return reject(err);
+                return Promise.reject(err);
               }
-              return self.gfs.collection(self.opts.bucket).distinct('filename', {
-                'metadata.dirname': dirname
-              }, function(err, files) {
-                if (err) {
-                  return reject(err);
-                }
-                return fulfill(files);
-              });
+              return Promise.resolve(files);
             });
-          })["catch"](reject);
+          });
         });
-        if (cb) {
-          ret.nodeify(cb);
-          return this;
-        }
-        return ret;
       },
-      read: function(fd, cb) {
+      read: function(fd) {
         return this.readLastVersion(fd, cb);
       },
-      readLastVersion: function(fd, cb) {
+      readLastVersion: function(fd) {
         return this.readVersion(fd, -1, cb);
       },
-      readVersion: function(fd, version, cb) {
-        var ret;
-        ret = new Promise(function(fulfill, reject) {
-          return self.conn.then(function() {
-            return self.gfs.collection(self.opts.bucket).find({
-              filename: fd
-            }).limit(-1).skip(version < 0 ? Math.abs(version) - 1 : version).sort({
-              uploadDate: version < 0 ? -1 : 1
-            }).next(function(err, file) {
-              var gridStore;
-              if (err) {
-                console.log(err);
-                return reject(err);
-              }
-              if (!file) {
-                err = new Error('ENOENT');
-                _.extend(err, {
-                  name: 'Error (ENOENT)',
-                  code: 'ENOENT',
-                  status: 404,
-                  message: "No file exists in this mongo gridfs bucket with that file descriptor (" + fd + ")"
-                });
-                return reject(err);
-              }
-              gridStore = new GridStore(self.db, file._id, 'r', {
-                root: self.opts.bucket
+      readVersion: function(fd, version) {
+        return self.conn.then(function() {
+          return self.gfs.collection(self.opts.bucket).find({
+            filename: fd
+          }).limit(-1).skip(version < 0 ? Math.abs(version) - 1 : version).sort({
+            uploadDate: version < 0 ? -1 : 1
+          }).next(function(err, file) {
+            var gridStore;
+            if (err) {
+              return Promise.reject(err);
+            }
+            if (!file) {
+              err = new Error('ENOENT');
+              _.extend(err, {
+                name: 'Error (ENOENT)',
+                code: 'ENOENT',
+                status: 404,
+                message: "No file exists in this mongo gridfs bucket with that file descriptor (" + fd + ")"
               });
-              return gridStore.open(function(err, gridStore) {
-                if (err) {
-                  return reject(err);
-                }
-                stream = gridStore.stream();
-                stream.pipe(concat(fulfill));
-                return stream.on('error', reject);
-              });
+              return Promise.reject(err);
+            }
+            gridStore = new GridStore(self.db, file._id, 'r', {
+              root: self.opts.bucket
             });
-          })["catch"](reject);
+            return gridStore.open(function(err, gridStore) {
+              if (err) {
+                return reject(err);
+              }
+              stream = gridStore.stream();
+              stream.pipe(concat(Promise.resolve));
+              return stream.on('error', Promise.reject);
+            });
+          });
         });
-        if (cb) {
-          ret.nodeify(cb);
-          return this;
-        }
-        return ret;
       },
-      rm: function(fd, cb) {
-        var ret;
-        ret = new Promise(function(fulfill, reject) {
-          return self.conn.then(function() {
-            return self.gfs.exist({
+      rm: function(fd) {
+        return self.conn.then(function() {
+          return self.gfs.exist({
+            filename: fd,
+            root: self.opts.bucket
+          }, function(err, found) {
+            if (err) {
+              return Promise.reject(err);
+            }
+            if (!found) {
+              return Promise.reject("" + fd + " not found");
+            }
+            return self.gfs.remove({
               filename: fd,
               root: self.opts.bucket
-            }, function(err, found) {
+            }, function(err) {
               if (err) {
-                return reject(err);
+                return Promise.reject(err);
               }
-              if (!found) {
-                return reject("" + fd + " not found");
-              }
-              return self.gfs.remove({
-                filename: fd,
-                root: self.opts.bucket
-              }, function(err) {
-                if (err) {
-                  return reject(err);
-                }
-                return fulfill();
-              });
+              return Promise.resolve();
             });
-          })["catch"](reject);
+          });
         });
-        if (cb) {
-          ret.nodeify(cb);
-          return this;
-        }
-        return ret;
       },
       receive: function(opts) {
         var Receiver;
